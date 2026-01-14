ce14660caa23b9b0f3fd1cc37b607da7f10aec29
Ângelo Azevedo <66081804+ang3lo-azevedo@users.noreply.github.com>
2026-01-14 15:57:11 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -6,19 +6,28 @@
 }:
 
 stdenv.mkDerivation {
-  name = "proton-cachyos";
-  version = "${protonCachyosVersions.base}-${protonCachyosVersions.release}";
+  pname = "proton-cachyos";
+  # Combining base and release for the nix version
+  version = "${protonCachyosVersions.base}.${protonCachyosVersions.release}";
 
   src = fetchurl {
-    url = "https://mirror.cachyos.org/repo/x86_64_v3/cachyos-v3/proton-cachyos-1%3A${protonCachyosVersions.base}.${protonCachyosVersions.release}-x86_64_v3.pkg.tar.zst";
+    # The URL needs the encoded colon %3A and the specific dash structure
+    # Updated to: proton-cachyos-1:BASE.RELEASE-2-x86_64_v3... 
+    # (Assuming the package release is usually '2' or '1')
+    url = "https://mirror.cachyos.org/repo/x86_64_v3/cachyos-v3/proton-cachyos-1%3A${protonCachyosVersions.base}.${protonCachyosVersions.release}-2-x86_64_v3.pkg.tar.zst";
     inherit (protonCachyosVersions) hash;
   };
 
   nativeBuildInputs = [ zstd ];
 
-  installPhase = ''
+  unpackPhase = ''
+    # Use -I zstd for tar to handle the compression
     tar -I zstd -xf $src
+  '';
+
+  installPhase = ''
     mkdir -p $out/share/steam/compatibilitytools.d
+    # CachyOS packages usually have a 'usr' folder inside the tarball
     mv usr/share/steam/compatibilitytools.d/proton-cachyos $out/share/steam/compatibilitytools.d/
   '';
 
